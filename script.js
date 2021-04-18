a26b7b096841a1d5c29076c02b028916798d85d3
Joshua Malone <jcharlesmalone@gmail.com>
2021-04-17 21:34:58 -0700

    4

---
+++ script.js
@@ -0,0 +1,9 @@
+var curentHumidity= document.getElementById("currentHumidity");
+var currentTemp = document.getElementById("currentTemp");
+var currentWindspeed = document.getElementById("currentWindspeed");
+var humidity= document.getElementById("humidity");
+var temp = document.getElementById("temp");
+var windspeed = document.getElementById("windspeed");
+var nameofCity = document.getElementById("nameofCity")
+
+
